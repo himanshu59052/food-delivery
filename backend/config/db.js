1157fa950980f3d1a3ca7a54403477bc09e8e9c9
Himanshu Kumar <110041943+himanshu59052@users.noreply.github.com>
2024-08-12 23:14:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-   await mongoose.connect('mongodb+srv://himanshukr:098890@cluster0.mpkg7xj.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
+   await mongoose.connect('mongodb+srv://himanshukr:098890@cluster0.mpkg7xj.mongodb.net/food-delivery').then(()=>console.log("DataBase Connected"));
 }
